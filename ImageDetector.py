408efb71667a70e6b954da18872c3d866ce59ed0
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-10-20 02:17:18 +0900

    Update ImageDetector.py

--- ImageDetector.py
+++ ImageDetector.py
@@ -4,25 +4,44 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
-
 customDetector = tf.saved_model.load('customDetector/saved_model')
 
-ambulance_rectCol = (255, 51, 000)
-cane_rectCol = (204, 51, 000)
-wheelchair_rectCol = (204, 51, 000)
-baby_carriage_rectCol = (204, 51, 000)
+# ================== [ CONFIG ] ================
+
+DETECTION_BOX_FONT = cv2.FONT_HERSHEY_SIMPLEX
+DETECTION_BOX_LINE = cv2.LINE_AA
+
+DETECTION_PRECISION = 0.5
+DETECTION_CLASS = [1]
+CUSTOM_DETECTOR_PRECISION = 0.3
+
+WHEELCHAIR_COLOR = (204, 51, 000)
+BABY_CARRIAGE_COLOR = (204, 51, 000)
+CANE_COLOR = (204, 51, 000)
+AMBULANCE_COLOR = (255, 51, 000)
+
+WHEELCHAIR_TEXT = "Wheelchair"
+BABY_CARRIAGE_TEXT = "Baby_carriage"
+CANE_TEXT = "Cane"
+AMBULANCE_TEXT = "Ambulance"
+
+WHEELCHAIR_CLASS = 0
+BABY_CARRIAGE_CLASS = 1
+CANE_CLASS = 2
+AMBULANCE_CLASS = 3
+
+# Please Check :: LabelMap Index = Real Class Number - 1
+LabelMap = [[WHEELCHAIR_TEXT, WHEELCHAIR_COLOR],
+            [BABY_CARRIAGE_TEXT, BABY_CARRIAGE_COLOR],
+            [CANE_TEXT, CANE_COLOR],
+            [AMBULANCE_TEXT, AMBULANCE_COLOR]]
 
 
+# ===============================================
+
+# image = Pure Image that not drew anything
 def Detector(image):
-    # Convert img to RGB
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Is optional but i recommend (float conversion and convert img to tensor image)
-    rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
-
-    # Add dims to rgb_tensor
-    rgb_tensor = tf.expand_dims(rgb_tensor, 0)
-
+    rgb_tensor = cvImgToRGBTensor(image)
     boxes, scores, classes, num_detections = detector(rgb_tensor)
 
     pred_labels = classes.numpy().astype('int')[0]
@@ -34,12 +53,12 @@ def Detector(image):
 
     # loop throughout the detections and place a box around it
     for score, (ymin, xmin, ymax, xmax), label in zip(pred_scores, pred_boxes, pred_labels):
-        if score < 0.5 or label != 1:
+        if score < DETECTION_PRECISION or label not in DETECTION_CLASS:
             continue
 
         img_result = cv2.rectangle(img_result, (xmin, ymax), (xmax, ymin), (0, 255, 0), 1)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img_result, 'person', (xmin, ymax - 10), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(img_result, 'person', (xmin, ymax - 10), DETECTION_BOX_FONT, 0.5, (255, 0, 0), 1,
+                    DETECTION_BOX_LINE)
 
         pos_result.append([xmin, ymax, xmax, ymin])
 
@@ -49,14 +68,7 @@ def Detector(image):
 # image = Pure Image that not drew anything
 # drawOnImg = image that you gonna draw something on
 def CustomDetector(image, drawOnImg=None):
-    # Convert img to RGB
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Is optional but i recommend (float conversion and convert img to tensor image)
-    rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
-
-    # Add dims to rgb_tensor
-    rgb_tensor = tf.expand_dims(rgb_tensor, 0)
+    rgb_tensor = cvImgToRGBTensor(image)
     output_dict = customDetector(rgb_tensor)
 
     num_detections = int(output_dict.pop('num_detections'))
@@ -74,17 +86,16 @@ def CustomDetector(image, drawOnImg=None):
     h, w, c = image.shape
     img_result = image
 
-    ambulance_pos = []
-    cane_pos = []
-    wheelchair_pos = []
-    baby_carriage_pos = []
+    pos_result = []
+    for i in range(0, len(LabelMap)):
+        pos_result.append([])
 
     if drawOnImg is not None:
         img_result = drawOnImg
 
     # loop throughout the detections and place a box around it
     for score, (ymin, xmin, ymax, xmax), label in zip(pred_scores, pred_boxes, pred_labels):
-        if score < 0.3:
+        if score < CUSTOM_DETECTOR_PRECISION:
             continue
 
         y_min = int(ymin * h)
@@ -92,33 +103,23 @@ def CustomDetector(image, drawOnImg=None):
         y_max = int(ymax * h)
         x_max = int(xmax * w)
 
-        try:
-            label_txt = ''
+        label_txt = LabelMap[label - 1][0]
+        img_result = cv2.rectangle(img_result, (x_min, y_max), (x_max, y_min), LabelMap[label - 1][1], 3)
+        pos_result[label - 1].append([x_min, y_max, x_max, y_min])
+        cv2.putText(img_result, label_txt, (x_min, y_max - 10),
+                    DETECTION_BOX_FONT, 0.5, (255, 0, 0), 3, DETECTION_BOX_LINE)
 
-            if label == 1:
-                label_txt = 'wheelchair'
-                img_result = cv2.rectangle(img_result, (x_min, y_max), (x_max, y_min), wheelchair_rectCol, 3)
-                wheelchair_pos.append([x_min, y_max, x_max, y_min])
+    return img_result, pos_result
 
-            elif label == 2:
-                label_txt = 'baby_carriage'
-                img_result = cv2.rectangle(img_result, (x_min, y_max), (x_max, y_min), baby_carriage_rectCol, 3)
-                baby_carriage_pos.append([x_min, y_max, x_max, y_min])
 
-            elif label == 3:
-                label_txt = 'cane'
-                img_result = cv2.rectangle(img_result, (x_min, y_max), (x_max, y_min), cane_rectCol, 3)
-                cane_pos.append([x_min, y_max, x_max, y_min])
+def cvImgToRGBTensor(image):
+    # Convert img to RGB
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            elif label == 4:
-                label_txt = 'ambulance'
-                img_result = cv2.rectangle(img_result, (x_min, y_max), (x_max, y_min), ambulance_rectCol, 3)
-                ambulance_pos.append([x_min, y_max, x_max, y_min])
+    # Is optional but i recommend (float conversion and convert img to tensor image)
+    rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
 
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(img_result, label_txt, (x_min, y_max - 10), font, 0.5, (255, 0, 0), 3, cv2.LINE_AA)
+    # Add dims to rgb_tensor
+    rgb_tensor = tf.expand_dims(rgb_tensor, 0)
 
-        except Exception as e:
-            print(e)
-
-    return img_result, ambulance_pos, cane_pos, wheelchair_pos, baby_carriage_pos
+    return rgb_tensor
