42a83ef8df5a3c6728de5226fdd113fffb04e4d3
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-10-22 21:32:25 +0900

    Error Fix
    Fixed Fatal Error that cannot receive any data from Server.

--- DetectorConnector.py
+++ DetectorConnector.py
@@ -1,8 +1,10 @@
 import socket
-import numpy
+import json
 import time
 import base64
 import sys
+import numpy
+import cv2
 
 
 def receiveAll(sock, count):
@@ -19,6 +21,7 @@ def receiveAll(sock, count):
     return buf
 
 
+'''
 def clearBuffer(sock):
     sock.settimeout(0.1)
     while True:
@@ -27,21 +30,21 @@ def clearBuffer(sock):
         except socket.timeout:
             break
     sock.settimeout(0.5)
+'''
 
 
 class Connector:
 
     def __init__(self, ip, port):
         self.notReceived_Before = False
-        self.sock = socket.socket()
         self.TCP_SERVER_IP = ip
         self.TCP_SERVER_PORT = port
         self.connectCount = 0
-        self.sock.settimeout(0.5)
         self.connectServer()
 
     def connectServer(self):
         try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((self.TCP_SERVER_IP, self.TCP_SERVER_PORT))
             print(
                 u'Client socket is connected with Server socket [ TCP_SERVER_IP: ' + self.TCP_SERVER_IP
@@ -58,30 +61,30 @@ class Connector:
 
     def processing(self, image):
         try:
-            data = numpy.array(image)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+            _, img_encode = cv2.imencode('.jpg', image, encode_param)
+            data = numpy.array(img_encode)
             stringData = base64.b64encode(data)
             length = str(len(stringData))
-
-            if self.notReceived_Before:
-                clearBuffer(self.sock)
 
             self.sock.sendall(length.encode('utf-8').ljust(64))
             self.sock.send(stringData)
 
-            result_len = receiveAll(self.sock, 66)
-            result_len = result_len.decode('utf-8')
+            result_len = receiveAll(self.sock, 64)
 
             if result_len is None:
-                self.notReceived_Before = True
                 return None
 
+            result_len = result_len.decode('utf-8')
             result = receiveAll(self.sock, int(result_len))
 
             if len(result) == 0:
                 return None
 
-            result = numpy.frombuffer(base64.b64decode(result), numpy.uint8)
-            return result
+            result_decoded = result.decode('utf-8')
+            result_list = json.loads(result_decoded)
+
+            return result_list
 
         except Exception as e:
             print(e)
@@ -89,3 +92,14 @@ class Connector:
             time.sleep(1)
             self.connectServer()
             self.processing()
+
+    def disconnect(self):
+        self.sock.close()
+
+'''
+if __name__ == "__main__":
+    c = Connector('localhost', 7777)
+    img = cv2.imread('./Images/Test/test_custom_detector.jpg')
+    result = c.processing(img)
+    print('result-->', result)
+'''
