4c23f589c287ca7bb3d4a8d2f19351fa91c1b29e
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-10-22 21:33:39 +0900

    Error Fix
    Fixed Fatal Error that Server can not receieve any data from Client ( = DetectorConnector )

--- DetectorServer.py
+++ DetectorServer.py
@@ -1,7 +1,8 @@
 import socket
 import numpy
 import base64
-import threading
+import cv2
+import json
 
 import ImageDetector
 
@@ -24,8 +25,7 @@ class DetectorServer:
         self.TCP_PORT = port
         self.folder_num = 0
         self.socketOpen()
-        self.receiveThread = threading.Thread(target=self.receiveImages)
-        self.receiveThread.start()
+        self.receiveImages()
 
     def socketClose(self):
         self.sock.close()
@@ -44,33 +44,41 @@ class DetectorServer:
         try:
             while True:
                 length = receiveAll(self.conn, 64)
+                if length is None:
+                    continue
                 length1 = length.decode('utf-8')
                 stringData = receiveAll(self.conn, int(length1))
                 data = numpy.frombuffer(base64.b64decode(stringData), numpy.uint8)
-                result = self.processing(data)
+                img = cv2.imdecode(data, 1)
+                result = self.processing(img)
                 self.sendResult(result)
 
         except Exception as e:
             print(e)
             self.socketClose()
             self.socketOpen()
-            self.receiveThread = threading.Thread(target=self.receiveImages)
-            self.receiveThread.start()
+            self.receiveImages()
 
     # data must be numpy array
     def processing(self, data):
-        image_result, pos_result = ImageDetector.Detector(data)
-        image_result, custom_result = ImageDetector.CustomDetector(data, image_result)
-        return [image_result, pos_result, custom_result]
+        _, pos_result = ImageDetector.Detector(data)
+        _, custom_result = ImageDetector.CustomDetector(data)
+        return [pos_result, custom_result]
 
     def sendResult(self, result):
-        result_np = numpy.array(result)
-        result_encoded_np = base64.b64encode(result_np)
-        length = str(len(result_encoded_np))
-        self.sock.sendall(length.encode('utf-8').ljust(64))
-        self.conn.send(result_encoded_np)
 
-'''
+        print('result-->', result)
+
+        result_encoded_json = json.dumps(result).encode('utf-8')
+        length = str(len(result_encoded_json))
+
+        self.conn.sendall(length.encode('utf-8').ljust(64))
+        self.conn.send(result_encoded_json)
+
+
 def main():
-    server = DetectorServer('localhost', 8080)
-'''
+    server = DetectorServer('localhost', 7777)
+
+
+if __name__ == "__main__":
+    main()
