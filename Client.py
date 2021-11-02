84db92a1825e67aa6c44e74306a347bfd931ccb3
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-11-02 23:05:06 +0900

    Update Client.py

--- Client.py
+++ Client.py
@@ -13,12 +13,15 @@ if __name__ == "__main__":
     IP = args.ip
     PORT = args.port
 
-    print('\n\n ==============================================\n'
-          ' Initializing THE-CROSS CLIENT PROGRAM\n',
-          'Detector Server IP : ', IP, "\n",
-          'Detector Server PORT : ', PORT, '\n',
-          '==============================================\n\n',
-          '*** It takes a long time, so please wait. ***\n\n')
+    print('\n\n ==================== [ THE CROSS CLIENT ] ====================\n'
+          '            - Initializing THE-CROSS CLIENT PROGRAM\n',
+          '           - Detector Server IP : ', IP, "\n",
+          '           - Detector Server PORT : ', PORT, '\n',
+          '==============================================================\n\n',
+          '========================= [!CAUTION!] ========================\n',
+          '>>> DONT CLOSE PROGRAM UNTIL SUCCESSFULLY CAMERA CONNECTED <<<\n',
+          '==============================================================\n\n')
+
 
     import main
     main.start(IP, PORT)
