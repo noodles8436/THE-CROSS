b60d89b3f43e08f8f59b2bded9e43c0419a31a97
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-11-04 02:46:58 +0900

    Annotation Enhancement

--- SirenDetector.py
+++ SirenDetector.py
@@ -5,11 +5,22 @@ import sounddevice as sd
 import numpy as np
 
 siren_classesNum = [316, 317, 318, 319, 390, 391]
+"""The siren classification number list used by yamnet."""
 
 
 class SirenDetector(threading.Thread):
+    """This class is for recognizing the siren sound of an emergency vehicle.
+    """
 
     def __init__(self):
+        """A function to initialize the SirenDetector class
+        Create and store objects to use the yamnet model of the tensorflow-hub module.
+        Save setting values for receiving sound input.
+        Create a flag variable that determines whether the siren detection function operates.
+        Importantly, Create an 'self.isSirenDetected' variable in the SirenDetector
+        so that MainClass can access whether a siren is detected.
+        """
+
         super().__init__()
         self.model = hub.load('https://tfhub.dev/google/yamnet/1')
         self.fs = 16000
@@ -18,6 +29,14 @@ class SirenDetector(threading.Thread):
         self.isSirenDetected = False
 
     def run(self):
+        """This function is a function that detects sirens.
+        While constantly detecting the siren,
+        change the isSerenDetected variable depending on whether Siren is detected or not.
+
+        If there is a siren among the top five classification results predicted by yamnet, the siren detection model,
+        we implemented that the siren sound detected.
+        """
+
         self.isRun = True
         while self.isRun:
             result = sd.rec(self.duration * self.fs, samplerate=self.fs, channels=1, dtype=np.float32).reshape(-1)
@@ -34,7 +53,11 @@ class SirenDetector(threading.Thread):
             self.isSirenDetected = siren_result
 
     def isSiren(self):
+        """This function returns whether a siren is detected.
+        """
         return self.isSirenDetected
 
     def stop(self):
+        """This function stops the siren detector.
+        """
         self.isRun = False
