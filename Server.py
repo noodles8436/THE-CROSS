07aa9b721c88d12766682cad1ac5ac65e42e867d
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-11-04 02:46:34 +0900

    Annotation Enhancement

--- Server.py
+++ Server.py
@@ -1,7 +1,21 @@
 import argparse
 
-if __name__ == "__main__":
 
+def startServer():
+    """This function starts the Detection Server to receive an image from the Client
+    and send the detection result of the Object Detection to the Client.
+
+    To run Server.py, enter IP and PORT information of server in the console window
+    to Open Object Detection Server
+
+    .. note:: The default IP of the Server is localhost.
+    .. note:: The default PORT of the Server is 7777.
+    .. note:: DO NOT CLOSE PROGRAM UNTIL Socket Opened Message printed
+
+        Example:
+            Run Server.py on the console in the following format:
+            >> python Server.py --ip=XX.XX.XX.XX --port=XXX
+    """
     parser = argparse.ArgumentParser(description='THE-CROSS DetectorServer arg parser')
     parser.add_argument('--ip', type=str, help='Input Detection Server IP\n ex) 127.0.0.1',
                         default='localhost', required=False)
@@ -22,3 +36,7 @@ if __name__ == "__main__":
 
     from DetectorServer import DetectorServer
     server = DetectorServer(IP, PORT)
+
+
+if __name__ == "__main__":
+    startServer()
