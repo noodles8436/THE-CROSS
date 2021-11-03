02f0c9774ed2ba75e6c12c4a378d9fb07b48c99a
noodles8436 <88359959+noodles8436@users.noreply.github.com>
2021-11-04 02:45:26 +0900

    Annotation Enhancement

--- ImageDetector.py
+++ ImageDetector.py
@@ -27,7 +27,7 @@ BABY_CARRIAGE_CLASS = 1
 CANE_CLASS = 2
 AMBULANCE_CLASS = 3
 
-# Please Check :: LabelMap Index = Real Class Number - 1
+# Please Check :: LabelMap Index = Object detection Model's Class Number - 1
 LabelMap = [[WHEELCHAIR_TEXT, WHEELCHAIR_COLOR],
             [BABY_CARRIAGE_TEXT, BABY_CARRIAGE_COLOR],
             [CANE_TEXT, CANE_COLOR],
@@ -36,18 +36,28 @@ LabelMap = [[WHEELCHAIR_TEXT, WHEELCHAIR_COLOR],
 
 # ===============================================
 
-# image = Pure Image that not drew anything
 def Detector(image):
+    """This function detects objects in the image using the tensorflow-hub module. And return the detection result.
+
+    :param 3D-ndarray image: 3D ndarray type data, which has image data.
+
+    :returns: 2D list [[xmin, ymax, xmax, ymin], [..], ..]
+    """
+
+    # Convert 3D ndarray image into tensor array ( 3D array = RBG )
     rgb_tensor = cvImgToRGBTensor(image)
+
+    # Save object detection result using tensorflow-hub module
     boxes, scores, classes, num_detections = detector(rgb_tensor)
 
     pred_labels = classes.numpy().astype('int')[0]
     pred_boxes = boxes.numpy()[0].astype('int')
     pred_scores = scores.numpy()[0]
 
+    # Create variable to return
     pos_result = []
 
-    # loop throughout the detections and place a box around it
+    # The pos_result list stores the object location only when the detected object is one of DETECTION_CLASS.
     for score, (ymin, xmin, ymax, xmax), label in zip(pred_scores, pred_boxes, pred_labels):
         if score < DETECTION_PRECISION or label not in DETECTION_CLASS:
             continue
@@ -57,9 +67,19 @@ def Detector(image):
     return pos_result
 
 
-# image = Pure Image that not drew anything
 def CustomDetector(image):
+    """This function detects objects in the image using the Transfer learned Object Detection Model.
+    And return the detection result.
+
+    :param 3D-ndarray image: 3D ndarray type data, which has image data.
+
+    :returns: 3D list [ [[xmin, ymax, xmax, ymin], [..], ..], [[xmin, ymax, xmax, ymin], [..], ..], ..]
+    """
+
+    # Convert 3D ndarray image into tensor array ( 3D array = RBG )
     rgb_tensor = cvImgToRGBTensor(image)
+
+    # Save object detection result using customDetector ( which is Transfer learned Object Detection Model. )
     output_dict = customDetector(rgb_tensor)
 
     num_detections = int(output_dict.pop('num_detections'))
@@ -74,13 +94,16 @@ def CustomDetector(image):
     pred_boxes = output_dict['detection_boxes']
     pred_scores = output_dict['detection_scores']
 
+    # Save the resolution of the image to calculate detected object's position
     h, w, c = image.shape
 
+    # Create variable to return
     pos_result = []
+
     for i in range(0, len(LabelMap)):
         pos_result.append([])
 
-    # loop throughout the detections and place a box around it
+    # The pos_result list stores the object location
     for score, (ymin, xmin, ymax, xmax), label in zip(pred_scores, pred_boxes, pred_labels):
         if score < CUSTOM_DETECTOR_PRECISION:
             continue
@@ -96,6 +119,13 @@ def CustomDetector(image):
 
 
 def cvImgToRGBTensor(image):
+    """This function converts 3D-ndarray data into tensor-array data. ( 3D Array = RGB )
+
+    :param 3D-ndarray image: 3D ndarray type data, which has image data.
+
+    :returns: tensor-array data ( 3D Array = RGB )
+
+    """
     # Convert img to RGB
     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
